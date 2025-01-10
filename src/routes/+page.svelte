442364bdfe1e828154d5f1cc1c4521f5a260ffe9
lemon-gith <77743782+lemon-gith@users.noreply.github.com>
2025-01-10 15:05:11 +0000

    fix navbar gap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
 <h1>Pierce Wiegerling</h1>
-<p>Visit <a href="https://youtu.be/dQw4w9WgXcQ">this page</a> for fun</p>
+<p>Visit <a href="https://youtu.be/dQw4w9WgXcQ" class="link">this page</a> for fun</p>
+<p>Visit <a href="https://youtu.be/dQw4w9WgXcQ" class="link">this page</a> for fun</p>
+<p>Visit <a href="https://youtu.be/dQw4w9WgXcQ" class="link">this page</a> for fun</p>
+<p>Visit <a href="https://youtu.be/dQw4w9WgXcQ" class="link">this page</a> for fun</p>
+<p>Visit <a href="https://youtu.be/dQw4w9WgXcQ" class="link">this page</a> for fun</p>
+<p>Visit <a href="https://youtu.be/dQw4w9WgXcQ" class="link">this page</a> for fun</p>
